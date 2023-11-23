0f27e4b402d8b2a6708a36152784032c9732c75a
ubc-tuehoang <86985864+ubc-tuehoang@users.noreply.github.com>
2023-11-22 16:47:42 -0800

    Update bard-api.py

--- bard-api.py
+++ bard-api.py
@@ -1,7 +1,7 @@
 from bardapi import Bard
 
 ##Secure-1PSID
-token = 'dQiGCsNVB7D_2td9TJ-ErRUsNOfOT0zjNyfExecnCzXAlAUH4oQwGo2JSC9QNHZTafhKSQ.'
+token = '[token psid]'
 bard = Bard(token=token)
 queryString = 'You are an advisor for university. Create and advise for undergrade Business courses at UBC.'
 
